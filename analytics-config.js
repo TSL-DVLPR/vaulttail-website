a1881ac29a1fd90839e016dc6ccc1c4367a984dd
TSL-DVLPR <310341463+TSL-DVLPR@users.noreply.github.com>
2026-09-19 22:41:57 -0700

    Set GA4 Measurement ID in analytics config

--- analytics-config.js
+++ analytics-config.js
@@ -3,6 +3,6 @@
 // Microsoft Clarity: Project > Settings > Setup > ID in clarity.ms/tag/....
 // Empty values keep the optional services disabled.
 window.VAULTTAIL_ANALYTICS = Object.freeze({
-  ga4MeasurementId: "",
+  ga4MeasurementId: "G-3LM3HJ81YH",
   clarityProjectId: "",
 });
